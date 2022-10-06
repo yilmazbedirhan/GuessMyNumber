201868808235b8258640d79c466af814793e6ab8
yilmazbedirhan <yilmazbedirhan35@gmail.com>
2022-10-06 20:55:19 +0300

    start game screen styling

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <StartGameScreen />
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -1,10 +1,14 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet, Pressable } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -12,3 +16,25 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#AC44F2",
+    marginHorizontal: 30,
+    borderRadius: 15,
+    elevation: 15,
+  },
+  textInput: {
+    height: 50,
+    width: 100,
+    fontSize: 32,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FAD800",
+    color: "#FAD800",
+    marginVertical: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
